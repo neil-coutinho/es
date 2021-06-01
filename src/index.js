71ad108c96946dad349ac146caaad5d78d6d242a
Neil Coutinho <njcoutinho@gmail.com>
2021-06-01 15:36:56 +0530

    Generators setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+// eslint-disable-next-line no-console
 console.log('Hello World!');
+/* eslint-disable */
+console.log("GENERATORS");
+
+function getPosts(id) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((data) => console.log(data))
+}
+
+
+function* setup(id) {
+    //get post
+    //get post comments
+    const post = yield getPosts(id);
+    console.log(post);
+}
+
+const postData = setup();
+
+postData(1);

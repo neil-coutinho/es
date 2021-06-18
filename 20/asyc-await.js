cfa5c6f2a0945c771e1b921e53f37c7948b95b88
Neil Coutinho <njcoutinho@gmail.com>
2021-06-18 11:09:36 +0530

    Async await with try/catch error handling

--- 20/asyc-await.js
+++ 20/asyc-await.js
@@ -1,7 +1,7 @@
 function counter(count) {
     return new Promise((resolve, reject) => {
         if(count < 5) {
-            return reject("Oh Noooo!!! Value is < 5")
+            return reject(`Oh Noooo!!! Value ${count} is < 5`)
         }
 
         return setTimeout(()=> resolve(`Done after ${count} seconds`), (count*1000));
@@ -14,3 +14,15 @@ counter(5)
 counter(2)
     .then(res => console.log(res))
     .catch(e => console.log(e))
+
+async function count(seconds) {
+    try {
+        const value = await counter(seconds);
+        console.log(value);
+    } catch(e) {
+        console.log(e);
+    }
+   
+}
+
+count(4);

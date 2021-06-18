313b5b4b0d936ca6f9b4cbb01269227e3e42d4b9
Neil Coutinho <njcoutinho@gmail.com>
2021-06-18 11:19:19 +0530

    Async await error handling with higher order component

--- 20/asyc-await.js
+++ 20/asyc-await.js
@@ -15,14 +15,36 @@ counter(2)
     .then(res => console.log(res))
     .catch(e => console.log(e))
 
-async function count(seconds) {
-    try {
-        const value = await counter(seconds);
-        console.log(value);
-    } catch(e) {
-        console.log(e);
+function hoc(fn) {
+    return function(...args) {
+        fn(...args)
+        .catch(e => console.log(e));
     }
+
+}
+
+
+async function count(seconds) {
+    //Try/Catch error handling
+    // try {
+    //     const value = await counter(seconds);
+    //     console.log(value);
+    // } catch(e) {
+    //     console.log(e);
+    // }
+
+    //Catch 
+    // const value = await counter(seconds).catch(e => console.log(e));
+    // if(value) {
+    //     console.log(value);
+    // }
+   
+    const value = await counter(seconds);
    
 }
 
-count(4);
+// count(4);
+// count(10);
+
+const hocFn = hoc(count);
+hocFn(4);
